778520d503f1095941f3180dddadcec4973ac3e1
Alfredo Alas <alfredo.alas14@gmail.com>
2025-11-04 23:39:26 -0600

    add project files

---
+++ src/main/java/biz/utilsdev/security/AppSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package biz.utilsdev.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSecurityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/biz/utilsdev/security/AppSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.utilsdev.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
