5819a0ce4c9f2885dfa8582b1b6244b30eb09821
Alfredo Alas <alfredo.alas14@gmail.com>
2025-11-14 23:36:02 -0600

    chore(env): add dotenv-java to load environment variables from .env file

--- src/main/java/biz/utilsdev/security/AppSecurityServiceApplication.java
+++ src/main/java/biz/utilsdev/security/AppSecurityServiceApplication.java
@@ -1,5 +1,6 @@
 package biz.utilsdev.security;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppSecurityServiceApplication {
 
 	public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMissing()
+                .load();
+
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(AppSecurityServiceApplication.class, args);
 	}
 
