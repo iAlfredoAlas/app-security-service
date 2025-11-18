867c6070ab3540a2ee85bb4c6467c5a980b238bc
Alfredo Alas <alfredo.alas14@gmail.com>
2025-11-18 01:23:20 -0600

    feat(model): add Permission entity mapping for RBAC

--- src/main/java/biz/utilsdev/security/entity/Permission.java
+++ src/main/java/biz/utilsdev/security/entity/Permission.java
@@ -18,9 +18,9 @@ public class Permission implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
-    
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
