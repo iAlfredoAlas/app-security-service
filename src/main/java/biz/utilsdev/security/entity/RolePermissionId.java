7525ae8ba80bddd1a46a59e875ae4530c3e41dda
Alfredo Alas <alfredo.alas14@gmail.com>
2025-11-18 01:38:56 -0600

    feat(model): add embeddable composite key RolePermissionId

---
+++ src/main/java/biz/utilsdev/security/entity/RolePermissionId.java
@@ -0,0 +1,39 @@
+package biz.utilsdev.security.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+public class RolePermissionId implements Serializable {
+
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Column(name = "permission_id")
+    private Long permissionId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RolePermissionId that)) return false;
+        return Objects.equals(roleId, that.roleId) &&
+                Objects.equals(permissionId, that.permissionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleId, permissionId);
+    }
+
+}
